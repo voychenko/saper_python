d96a415356d44285c9c0b7caab43170b9ecd5e5a
voychenko <74076643+voychenko@users.noreply.github.com>
2020-11-24 14:52:55 +0200

    request add calculate bombs for cell

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ CELLSY=3
 BOMBCOUNT=4
 DISPLAYWIDTH=600
 DISPLAYHEIGHT=400
-img={"9":"closed.png","0":"numbers1.png"}
+img={"close":"closed.png","0":"numbers0.png","1":"numbers1.png","2":"numbers2.png","3":"numbers3.png","4":"numbers4.png","9":"bomb.png","flag":"flag.png"}
+
 
 #im0 = pygame.image.load("images//"+img[0])
 #im1 = pygame.image.load("images//"+img[1])
@@ -17,25 +18,51 @@ class cellsmine(object):
 	def __init__(self,dis, x,y):
 		self.x = x
 		self.y = y
-		self.currentstate=21 # close image 21 open umage  22 marked flag
+		self.currentstate=20 # close image 21 open umage  22 marked flag
 		self.disp=dis
 		self.choosenmark=0
 		self.cells_show()
 		
 	def onclick(self,x,y):
+
 		if (x>self.x and x<(self.x+RESOLUTION_IMAGE) ) and\
 		(y>self.y and y<(self.y+RESOLUTION_IMAGE) ):
-			if self.currentstate==22:
-				return False
-			if self.currentstate==21:
+			print (self)
+			print (self.currentstate)
+			print (self.choosenmark)
+			if self.currentstate==20: # cells close
+				self.currentstate=21 # mark open cells
 				if self.choosenmark<9:
-					self.currentstate=self.choosenmark
-					self.currentstate=22
+					self.cells_show()
 					return False
 				if self.choosenmark==9:
-					return True # return flag bomb activated
+					print ("Bomb")
+					self.cells_show()
+					return True
+			if self.currentstate==21:
+				return False
+
+	def onrightclick(self,x,y):
+		if (x>self.x and x<(self.x+RESOLUTION_IMAGE) ) and\
+		(y>self.y and y<(self.y+RESOLUTION_IMAGE) ):
+			if self.currentstate==20:
+				self.currentstate=22
+				self.cells_show()
+			elif self.currentstate==22:
+				self.currentstate=20
+				self.cells_show()
+
+
+
 	def cells_show(self):
-		self.disp.blit(pygame.image.load("images//"+img[str(self.choosenmark)]),(self.x,self.y))
+		if self.currentstate==21:
+			print("showbomb")
+			self.disp.blit(pygame.image.load("images//"+img[str(self.choosenmark)]),(self.x,self.y))
+		if self.currentstate==20:
+			self.disp.blit(pygame.image.load("images//"+img["close"]),(self.x,self.y))
+		if self.currentstate==22:
+			print("flag")
+			self.disp.blit(pygame.image.load("images//"+img["flag"]),(self.x,self.y))
 
 
 	def setmark_choosen(self,mark):
@@ -64,8 +91,8 @@ def game():
 		for i2 in range(0, CELLSY):
 			mass_array[i][i2].setx_choosen(i*RESOLUTION_IMAGE)
 			mass_array[i][i2].sety_choosen(i2*RESOLUTION_IMAGE)   #=cellsmine(dis,i*RESOLUTION_IMAGE,i2*RESOLUTION_IMAGE)
+			mass_array[i][i2].currentstate=20
 			mass_array[i][i2].cells_show()
-			print (mass_array[i][i2])
 	print("__________________")
 	# forming set with unical random number for bombs
 	count_mine=0
@@ -76,25 +103,26 @@ def game():
 			mass_array[i][i2].choosenmark=9
 			mass_array[i][i2].cells_show()
 			count_mine+=1
-			print(mass_array[i][i2])
-
-	#	if (d>CELLSX) and ((d+1)%(CELLSX+2)!=0) and (d%(CELLSX+1)!=CELLSX):
-	#		mass_mine.add(d)
-	#create freee mass for each cells, add firast+last row and add fist and last column with zero bombs for wase calculate numbers
-
- 	#firstcell=cellsmine(dis,40,40)
- 	#firstcell=cellsmine(dis,80,80)
  	pygame.display.flip()
  	game_over=False
 	while not game_over:
 		for event in pygame.event.get():
 		#	print(event)   #prints out all the actions that take place on the screen
  			pygame.display.update()
- 			#print(event)
+
  			if pygame.mouse.get_pressed()[0]:
  				clickpos= pygame.mouse.get_pos()
  				print (clickpos)
- 				firstcell.onclick(clickpos[0],clickpos[1])
+				for i in range(0, CELLSX):
+					for i2 in range(0, CELLSY):
+						mass_array[i][i2].onclick(clickpos[0],clickpos[1])
+ 			if pygame.mouse.get_pressed()[2]:
+ 				clickpos= pygame.mouse.get_pos()
+ 				print (clickpos)
+				for i in range(0, CELLSX):
+					for i2 in range(0, CELLSY):
+						mass_array[i][i2].onrightclick(clickpos[0],clickpos[1])
+
  			if event.type==pygame.QUIT:
  				game_over=True
 
