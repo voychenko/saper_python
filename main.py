973b5f118460942bca8ac0a605c4f0d3d23e1d10
voychenko <74076643+voychenko@users.noreply.github.com>
2020-11-24 17:01:15 +0200

    finished without  choose option

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import pygame
 from pygame.locals import *
 import random
 RESOLUTION_IMAGE=20
-CELLSX=3
-CELLSY=3
-BOMBCOUNT=4
-DISPLAYWIDTH=600
-DISPLAYHEIGHT=400
-img={"close":"closed.png","0":"numbers0.png","1":"numbers1.png","2":"numbers2.png","3":"numbers3.png","4":"numbers4.png","9":"bomb.png","flag":"flag.png"}
+CELLSX=10
+CELLSY=11
+BOMBCOUNT=3
+DISPLAYWIDTH=CELLSX*RESOLUTION_IMAGE
+DISPLAYHEIGHT=CELLSY*RESOLUTION_IMAGE
+img={"close":"closed.png","0":"numbers0.png","1":"numbers1.png","2":"numbers2.png","3":"numbers3.png","4":"numbers4.png","5":"numbers5.png","6":"numbers6.png","7":"numbers7.png","8":"numbers8.png","9":"bomb.png","flag":"flag.png"}
 
 
 #im0 = pygame.image.load("images//"+img[0])
@@ -27,9 +27,6 @@ class cellsmine(object):
 
 		if (x>self.x and x<(self.x+RESOLUTION_IMAGE) ) and\
 		(y>self.y and y<(self.y+RESOLUTION_IMAGE) ):
-			print (self)
-			print (self.currentstate)
-			print (self.choosenmark)
 			if self.currentstate==20: # cells close
 				self.currentstate=21 # mark open cells
 				if self.choosenmark<9:
@@ -82,7 +79,16 @@ def countbombs(aks):
 			e+=1
 	return e
 
+def opennext(mass_array,i,i2):
+	for d in range(-1,2):
+		for d1 in range (-1,2):
 
+				if ((i+d)>=0) and ((i2+d1)>=0) and ((i+d)<CELLSX) and ((i2+d1)<CELLSY) and (mass_array[i+d][i2+d1].currentstate==20):
+					mass_array[i+d][i2+d1].onclick((i+d)*RESOLUTION_IMAGE+1,(i2+d1)*RESOLUTION_IMAGE+1)
+					if mass_array[i+d][i2+d1].choosenmark==0:
+						print("recurcion")
+						opennext(mass_array,i+d,i2+d1)
+	return 
 
 def game():
 	pygame.init()
@@ -91,7 +97,7 @@ def game():
  	pygame.display.set_caption('Find bomb')
 	
 	
-	mass_array = [[cellsmine(dis,0,0) for i in range(0,CELLSX)] for i2 in range(0,CELLSY)]  
+	mass_array = [[cellsmine(dis,0,0) for i in range(0,CELLSY)] for i2 in range(0,CELLSX)]  
 
 	#forming full cells field
 	for i in range(0, CELLSX):
@@ -134,12 +140,15 @@ def game():
 				for i in range(0, CELLSX):
 					for i2 in range(0, CELLSY):
 						mass_array[i][i2].onclick(clickpos[0],clickpos[1])
+						if mass_array[i][i2].choosenmark==0:
+							opennext(mass_array,i,i2)
  			if pygame.mouse.get_pressed()[2]:
  				clickpos= pygame.mouse.get_pos()
  				print (clickpos)
 				for i in range(0, CELLSX):
 					for i2 in range(0, CELLSY):
 						mass_array[i][i2].onrightclick(clickpos[0],clickpos[1])
+
 
  			if event.type==pygame.QUIT:
  				game_over=True
